2273faf741e43b354e5c0d89970e0cc7eb571765
MenesiZoltan <colos.menesi@gmail.com>
2019-09-05 14:07:41 +0200

    First Commit on Dev

---
+++ DemoApp/DemoApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+            Console.WriteLine("Its a whole new World!");
+        }
+    }
+}
